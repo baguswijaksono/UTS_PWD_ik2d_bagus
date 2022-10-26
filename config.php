8a9f5e1226a5164bf3202317f81f78e67f593880
Bagus Muhammad Wijaksono <106671990+baguswijaksono@users.noreply.github.com>
2022-10-26 09:17:05 +0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('hostname', 'username', 'password', 'databasename');
-//Check for database connection error
-if($conn->connect_error){
-  die("Error in DB connection: ".$conn->connect_errno." : ".$conn->connect_error);    
-}
-?>
